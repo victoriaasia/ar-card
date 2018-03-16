37b85ba59740f56757118c9948ebd6ad8fed7c79
Королева Виктория Игоревна <Koroleva.VI@esphere.local>
2018-03-16 09:33:28 +0300

    .

--- src/js/script.js
+++ src/js/script.js
@@ -1,7 +1,9 @@
 "use strict"
 document.addEventListener("DOMContentLoaded", function(event) {
   let top = document.querySelector('.js-top');
+  let inner = document.querySelector('.js-inner');
   top.onclick = function() {
       top.classList.toggle('--animated');
+      inner.classList.toggle('--animated');
     }
   });
